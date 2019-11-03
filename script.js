fdeeaac3f3f327407f91ffdef8f3ee2a6a885668
eugborisov <eug.borisov91@gmail.com>
2019-11-04 01:44:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+  let windowsize = $(window).width();
+
+  $(window).resize(function() {
+    let windowsize = $(window).width();
+  });
+  if (windowsize > 576) {
+    $('.filters .dropdown').each(function() {
+      let width = $(this).children('.dropdown-menu').width();
+      $(this).css('width', width);
+    });
+  }
+
+  $('.filters .dropdown-item').click(function(e) {
+    e.preventDefault();
+    let name = $(this).children('.filters_name').text();
+    if ($(this).hasClass('bold')) {
+      $('.tags .tag_name').filter(function() {
+          return $(this).text() == name;
+      }).parent('li').remove();
+    } else {
+      $('.tags ul').append('<li><span class="tag_name">'+ name + '</span><span class="tag_cross">&#10005;</span></li>');
+    }
+    $(this).toggleClass('bold');
+  });
+
+  $('.filters .form-check-input').click(function() {
+    let name = $(this).siblings('.form-check-label').text();
+    if (!$(this).is(':checked')) {
+      $('.tags .tag_name').filter(function() {
+          return $(this).text() == name;
+      }).parent('li').remove();
+    } else {
+      $('.tags ul').append('<li><span class="tag_name">'+ name + '</span><span class="tag_cross">&#10005;</span></li>');
+    }
+  });
+
+  $('.tags').on('click', '.tag_cross', function() {
+    let li = $(this).parent('li');
+    let name = li.children('.tag_name').text();
+    $('.filters .dropdown-item').filter(function() {
+      return $(this).children('.filters_name').text() == name;
+    }).removeClass('bold');
+    $('.filters .form-check-input').filter(function() {
+      return $(this).siblings('.form-check-label').text() == name;
+    }).prop('checked', false);
+    li.remove();
+  });
+
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
